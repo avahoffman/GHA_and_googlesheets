14a2a359d25eb804de0ee4ad44e8241ccc0cbd17
avahoffman <avamariehoffman@gmail.com>
2024-09-09 14:21:47 -0400

    Update all to use safer GHA

--- R/render.R
+++ R/render.R
@@ -1,21 +1,24 @@
-# Render any materials relying on googlesheets4 package
+# Render any materials / analysis relying on non-interactive access to 
+# Google Sheets
 
-# Make sure to set appropriate secrets to allow auto-rendering to happen. 
+# Quick start
+
+# Make sure to set appropriate secrets to allow auto-rendering to happen.
 # This can be done via settings > secrets > actions menus.
 # Key should be generated in json format via the https://console.cloud.google.com/
 # interface, by creating a key that can be used by a Google Service Account.
 
-# Note that credentials need to be created in the workflows first. Any 
+# Note that credentials need to be passed in the workflows first. Any
 # Google Sheets in question must also be shared with the Google Service Account.
 
+# See https://www.avahoffman.com/GHA_and_googlesheets/ for the full guide
 
-install.packages(c("optparse", "jsonlite", "dplyr"),
-                 repos = "https://cloud.r-project.org")
-library(optparse)
-library(jsonlite)
-library(dplyr)
+install.packages("jsonlite", repos = "https://cloud.r-project.org")
 
-# --------- Get the GHA output ---------
+library(optparse) # make_option OptionParser parse_args
+library(jsonlite) # fromJSON
+
+# --------- Get the output from GHA ---------
 
 # Look for the data_in argument
 option_list <- list(
@@ -27,21 +30,30 @@ option_list <- list(
   )
 )
 
-# Read the resutlts provided as command line argument
+# Read the results provided as command line argument
 opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 jsonResults <- opt$data_in
 
 # --------- Interpret the JSON data ---------
 
+# Pull the data itself from the API results
 df <- fromJSON(jsonResults)
 df <- df$results$result$formatted[[2]]
 
 # Repair if you have column names on your spreadsheet
-colnames(df) <- df[1,] # colnames taken from first row of data
-df <- df[-1,] # remove the first row of data (original column names)
-df <- as_tibble(df)
+colnames(df) <- df[1, ] # colnames taken from first row of data
+df <- df[-1, ] # remove the first row of data (original column names)
+df <- tibble::as_tibble(df)
 
 # --------- Any analysis you want to do ---------
+# --------- Don't print sensitive data!! --------
 
-print(df)
+print(mean(df$`How would you rate our platform?`))
+
+# --------- Render web pages from Rmd docs ---------
+
+rmarkdown::render_site(
+  input = "pages",
+  envir = new.env(parent = globalenv()) # enable use of 'df' inside the Rmd being rendered
+) 
